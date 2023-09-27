7a4c3f139d0e01aa79cf23524704213b923fb97a
ryanchu04 <s10222093@connect.np.edu.sg>
2023-09-27 18:20:21 +0800

    Creating Button Function
    Understanding the button codes before adding other functions like the actions of what the button do

--- Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
+++ Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
@@ -11,7 +11,7 @@ namespace Valve.VR.InteractionSystem.Sample
     {
         public void OnButtonDown(Hand fromHand)
         {
-            ColorSelf(Color.cyan);
+            ColorSelf(Color.red);
             fromHand.TriggerHapticPulse(1000);
         }
 
