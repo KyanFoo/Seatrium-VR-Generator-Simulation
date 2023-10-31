e3cb813e0089554bf3fe7df87f550c2999d3e833
KyanFoo <S10223558@connect.np.edu.sg>
2023-10-31 08:51:02 +0800

    Testing Pointer

--- Assets/Scripts/CanvasInteraction/Pointer.cs
+++ Assets/Scripts/CanvasInteraction/Pointer.cs
@@ -16,13 +16,12 @@ public class Pointer : MonoBehaviour
     {
         //referencing line renderer in inspector
         lineRenderer = GetComponent<LineRenderer>();
-        //lineRenderer.enabled = false;
+        lineRenderer.enabled = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        /**
         Ray theRay = new Ray(transform.position, transform.forward);
 
         if (Physics.Raycast(theRay, out RaycastHit hit, defaultLength))
@@ -38,7 +37,6 @@ public class Pointer : MonoBehaviour
             }
 
         }
-        **/
 
         UpdateLine();
     }
