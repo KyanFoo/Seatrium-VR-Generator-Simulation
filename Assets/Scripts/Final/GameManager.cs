26b4af4bee39c5c0ec8a5662c43a9f8d435c25d0
KyanFoo <S10223558@connect.np.edu.sg>
2023-11-06 11:53:06 +0800

    Added Game Manager

---
+++ Assets/Scripts/Final/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Generator 1 Variables")]
+    public float Gen1Current;
+    public float Gen1Voltage;
+    public float Gen1Frequency;
+    public float Gen1Power;
+    [Header("Generator 2 Variables")]
+    public float Gen2Current;
+    public float Gen2Voltage;
+    public float Gen2Frequency;
+    public float Gen2Power;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
