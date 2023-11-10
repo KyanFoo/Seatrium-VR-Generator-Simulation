6ea5d6191c7fb66cefbc70492592848e63e0336f
KyanFoo <S10223558@connect.np.edu.sg>
2023-11-10 09:06:34 +0800

    Update Comments in "SynchroNeedle" Script
    Update and added more comments into the script.

--- Assets/Scripts/Needle/SynchroNeedle.cs
+++ Assets/Scripts/Needle/SynchroNeedle.cs
@@ -5,95 +5,115 @@ using UnityEngine;
 public class SynchroNeedle : MonoBehaviour
 {
     [Header("Pivot Transform GameObject")]
-    //Represent the GameObject that is being rotated.
+    //Represent the GameObject that is being rotated.//
     public Transform needle;
 
-    //Represent the transform Vector3 to check where the rotation starts and ends.
+    //Represent the transform Vector3 to check where the rotation starts and ends.//
     private Vector3 startRotation;
     private Vector3 endRotation;
 
-    //Represent the Duration of "Synchroscope Needle" rotating 360.
+    //Represent the Duration of "Synchroscope Needle" rotating 360.//
     private float lerpTime;
     public float lerpDuration; //**DO NOT WRITE ANYTHING INTO THIS INPUT**//
 
     [Header("Secondary Scripts")]
-    //Represent the "Secondary Script to access variables.
+    //Represent the "Secondary Script to access variables.//
     public SynchroscopeManager synchromanager;
 
-    //Represent the bool to check whether the syncroscope has been switched "on" and the isolator has been switched.
+    //Represent the bool to check whether the syncroscope has been switched "on" and the isolator has been switched.//
     private bool pauseSwitch;
     private bool startSwitch;
     private bool reverseSwitch;
 
-    //Represent the rotation of the Needle, either going Clockwise or Anti-Clockwise.
+    //Represent the rotation of the Needle, either going Clockwise or Anti-Clockwise.//
     private float rotation;
 
-    //Represent the rotation Vectors to lerp the Needle back to 12 "0" Clock.
+    //Represent the rotation Vectors to lerp the Needle back to 12 "0" Clock.//
     private Vector3 currentRotation;
     private float currentRotationZ;
     private Vector3 centerRotation;
 
+    [Header("ValueMeters Scripts & Settings")]
+    //Represent the scripts from ValueMeters.//
     public ValueMeter valueMeterFrequency1;
     public ValueMeter valueMeterFrequency2;
+
+    //Represent the variable value from the repesctive ValueMeters.//
     public float frequencyValue1;
     public float frequencyValue2;
 
-    //reference to blackout canvas
+    [Header("Blackout GameObject")]
+    //reference to blackout canvas.//
     public GameObject blackOut;
 
 
     private void Start()
     {
+        //Set the current rotation of the Synchroscope Needle.//
         startRotation = needle.rotation.eulerAngles;
+
+        //Set the rotation to clockwise (360f).//
         endRotation = startRotation + new Vector3(0, 0, rotation);
     }
 
     private void Update()
     {
-        //Retreive variable bool of respective bool switches.
+        //Check to see if the Isolator Switch has been switched "ON" or "OFF".//
         pauseSwitch = synchromanager.isolatorSwitch;
+
+        //Check to see if the Synchroscope has been switched "ON" or "OFF".//
         startSwitch = synchromanager.isNeedlePause;
+
+        //Check to see if the Reverse Switch has been switched "ON" or "OFF".//
         reverseSwitch = synchromanager.reverseLoop;
 
-        //Get the lerpDuration from the Manager script
-        //Make sure that the needle value is "1.3" more than the lamps.
-        //The needle movement is just right that is not too slow and fast and can have two lamps light up when it hit 12 o' clock.
+        //Get the lerpDuration from the Manager script.//
+        //Make sure that the needle value is "1.3" more than the lamps.//
+        //The needle movement is just right that is not too slow and fast and can have two lamps light up when it hit 12 o' clock.//
         lerpDuration = synchromanager.lerpDuration + 1.3f;
 
         if (startSwitch == true)
         {
-            //Rotate the Synchroscope Needle Clockwise.
-            //The reason why the supposed rotation is clockwise is -360 is because our game scene, everything is in reverse.
+            //Rotate the Synchroscope Needle Clockwise.//
+            //The reason why the supposed rotation is clockwise is -360 is because our game scene, everything is in reverse.//
             rotation = -360.0f;
             StartCoroutine(NeedleCoroutine());
         }
         if (startSwitch == true && reverseSwitch == true)
         {
-            //Rotate the Synchroscope Needle Anti-Clockwise.
+            //Rotate the Synchroscope Needle Anti-Clockwise.//
+            //The reason why the supposed rotation is Anti-clockwise is 360 is because our game scene, everything is in reverse.//
             rotation = 360.0f;
             StartCoroutine(NeedleCoroutine());
+
+            //Find its current transform rotation.//
             startRotation = needle.rotation.eulerAngles;
             lerpTime = 0;
         }
-        //Apply new rotation.
+        //Apply new rotation.//
         endRotation = startRotation + new Vector3(0, 0, rotation);
 
-        //Check whether the needle is within the deviation range.
+        //Check whether the needle is within the deviation range.//
         if (pauseSwitch == true)
         {
             currentRotation = needle.rotation.eulerAngles;
             if (currentRotation.z > 180)
             {
+                //Constantly update to check if needle GameObject had past the 180 rotation.//
+                //If it had when past already, it will rotate forward to the center.//
                 currentRotationZ = currentRotation.z;
                 centerRotation.z = 360;
                 currentRotationZ = currentRotation.z - 360;
             }
             else
             {
+                //Constantly update to check if needle GameObject had past the 180 rotation.//
+                //If it haven't past, it will rotate back its direction to the center.//
                 currentRotationZ = currentRotation.z;
                 centerRotation.z = 0;
             }
 
+            //Constantly update to check if variable value is within the deviation.//
             if (currentRotationZ >= -25 && currentRotationZ <= 25)
             {
                 Debug.Log("Within 20");
@@ -105,11 +125,14 @@ public class SynchroNeedle : MonoBehaviour
                 blackOut.SetActive(true);
             }
         }
+
+        //Constantly update to check variables value of the ValueMeters.//
         frequencyValue1 = valueMeterFrequency1.inputValue;
         frequencyValue2 = valueMeterFrequency2.inputValue;
+
+        //Check to see if Running Generator is bigger than Incoming Generators.//
         if (frequencyValue2 < frequencyValue1)
         {
-            //Debug.Log("Anti");
             synchromanager.reverseLoop = true;
         }
         else
