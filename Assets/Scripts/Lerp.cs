3fc5cc065a05e36539e0f01af3e73fbbde29a3dc
ryanchu04 <s10222093@connect.np.edu.sg>
2023-11-17 10:31:48 +0800

    Removed lamp lerping
    Lamp indicator for turning the generator on and off had a lerping effect that caused issues in the build, thus i removed the lerping effect but the indicator still works

--- Assets/Scripts/Lerp.cs
+++ Assets/Scripts/Lerp.cs
@@ -4,67 +4,24 @@ using UnityEngine;
 
 public class Lerp : MonoBehaviour
 {
-    private Renderer _renderCube;
-    public GameObject objCube;
-    public Color color;
+    public Material newMaterial;
+    public GameObject lights;
 
-    public Material targetMaterial;
-    private float startIntensity = 0.0f;
-    private float targetIntensity = 2.0f;
-    private float lerpDuration = 0.5f;
-
-    private float currentIntensity;
-    private float lerpTime;
-
-    public bool isOn;
-
-    private void Start()
+    public void ChangeMaterial()
     {
-        _renderCube = objCube.GetComponent<Renderer>();
-        _renderCube.material = targetMaterial;
-        targetMaterial.EnableKeyword("_EMISSION");
-        ///targetMaterial.SetColor("_EmissionColor", color * targetIntensity);
-        // Initialize the current intensity and lerp start time.
-        //currentIntensity = startIntensity;
-        lerpTime = 0;
-    }
+        // Get the Renderer component of the object to change material
+        Renderer renderer = lights.GetComponent<Renderer>();
 
-    private void Update()
-    {
-        isButtonPressed();
-    }
-
-    public void isButtonPressed()
-    {
-        if (isOn == true)
+        // Check if the Renderer component exists
+        if (renderer != null)
         {
-            LightUp();
+            // Assign the new material to the object
+            renderer.material = newMaterial;
         }
-        
-    }
-
-    public void isOnChecker()
-    {
-        isOn = true;
-    }
-    public void LightUp()
-    {
-        // Calculate the current time in the lerp.
-        lerpTime += Time.deltaTime;
-
-        // Calculate the new intensity using Mathf.Lerp.
-        currentIntensity = Mathf.Lerp(startIntensity, targetIntensity, lerpTime / lerpDuration);
-
-        // Apply the new emission intensity to the material.
-        targetMaterial.SetColor("_EmissionColor", color * currentIntensity);
-
-        // You may need to enable emission if it's not already.
-        targetMaterial.EnableKeyword("_EMISSION");
-
-        // Optionally, you can stop lerping when the target intensity is reached.
-        if (lerpTime >= lerpDuration)
+        else
         {
-            enabled = false;
+            // Log a warning if the Renderer component is not found
+            Debug.LogWarning("No Renderer on object");
         }
     }
 }
